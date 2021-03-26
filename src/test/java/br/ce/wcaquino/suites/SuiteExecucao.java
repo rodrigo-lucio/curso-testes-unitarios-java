0497f7bf8f6a1032accd4cf159a8d3cf3a4e1735
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-03-25 22:08:17 -0300

    Criado suite para testar testes em grupos e finalização do módulo 1 do curso

---
+++ src/test/java/br/ce/wcaquino/suites/SuiteExecucao.java
@@ -0,0 +1,35 @@
+package br.ce.wcaquino.suites;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.ce.wcaquino.servicos.CalculadoraTest;
+import br.ce.wcaquino.servicos.CalculoValorLocacaoTest;
+import br.ce.wcaquino.servicos.LocacaoServiceTest;
+
+/*
+ * Executa todos os testes de uma vez só
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+	CalculadoraTest.class,
+	CalculoValorLocacaoTest.class,
+	LocacaoServiceTest.class
+})
+public class SuiteExecucao {
+	
+	@BeforeClass
+	public static void before() {
+		//Executa tudo antes de toda a bateria
+		System.out.println("before");
+	}
+	
+	@AfterClass
+	public static void after() {
+		//Executa tudo depois de toda a bateria
+		System.out.println("after");
+	}
+}
