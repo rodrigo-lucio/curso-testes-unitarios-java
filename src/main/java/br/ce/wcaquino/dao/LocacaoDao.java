24a8af6eec4cba9617e2c907c81e1aaf4a9b6815
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-03-30 22:11:22 -0300

    Criado Claasses para simular dependencias externas

---
+++ src/main/java/br/ce/wcaquino/dao/LocacaoDao.java
@@ -0,0 +1,8 @@
+package br.ce.wcaquino.dao;
+
+import br.ce.wcaquino.entidades.Locacao;
+
+public interface LocacaoDao {
+
+	public void salvar(Locacao locacao);
+}
---
+++ src/main/java/br/ce/wcaquino/dao/LocacaoDaoFake.java
@@ -0,0 +1,10 @@
+package br.ce.wcaquino.dao;
+
+import br.ce.wcaquino.entidades.Locacao;
+
+public class LocacaoDaoFake implements LocacaoDao {
+
+	public void salvar(Locacao locacao) {
+	}
+
+}
