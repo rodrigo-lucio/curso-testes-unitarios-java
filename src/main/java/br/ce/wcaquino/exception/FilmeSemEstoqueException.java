3f50398fee3b315b433f5f394f6af69a57fd12e6
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-04-27 21:59:26 -0300

    Organização do código

--- src/main/java/br/ce/wcaquino/exception/FilmeSemEstoqueException.java
+++ src/main/java/br/ce/wcaquino/exception/FilmeSemEstoqueException.java
@@ -2,9 +2,6 @@ package br.ce.wcaquino.exception;
 
 public class FilmeSemEstoqueException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 }
--- src/main/java/br/ce/wcaquino/servicos/EmailService.java
+++ src/main/java/br/ce/wcaquino/servicos/EmailService.java
@@ -3,6 +3,5 @@ package br.ce.wcaquino.servicos;
 import br.ce.wcaquino.entidades.Usuario;
 
 public interface EmailService {
-
 	public void notificarAtraso(Usuario usuario);
 }
